035ccded2cd494245e0b74d712738bd980b0285d
14049 <14049@LAPTOP-VJ25A49D>
2020-10-20 01:49:56 -0400

    ugh

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -66,7 +66,7 @@ PlayMode::PlayMode() : scene(*phonebank_scene) {
 	//start player walking at nearest walk point:
 	player.at = walkmesh->nearest_walk_point(player.transform->position);
 
-	std::cout << "Why is 6 scared of 7? Because... "
+	std::cout << "Why is 6 scared of 7? Because... ";
 
 }
 
